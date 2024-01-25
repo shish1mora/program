b7b7aac3403c2e704f064db17ce59070bf40a564
Siegmeyer0of0Catarina <siegmeyerofcatarina0x@gmail.com>
2024-01-25 10:45:41 +0300

    Up2
    f

--- Exam-master/Exam/WordProcesser.cs
+++ Exam-master/Exam/WordProcesser.cs
@@ -29,48 +29,4 @@ namespace Exam
             Word.Application app = null;
             try
             {
-                app = new Word.Application();
-                Object file = _fileInfo.FullName;
-                Object missing = Type.Missing;
-                app.Documents.Open(file);
-                foreach(var item in dictionary)
-                {
-                    Word.Find find = app.Selection.Find;
-                    find.Text = item.Key;
-                    find.Replacement.Text = item.Value;
-                    Object wrap = Word.WdFindWrap.wdFindContinue;
-                    Object replace = Word.WdReplace.wdReplaceAll;
-                    find.Execute(FindText: Type.Missing,
-                        MatchCase: false,
-                        MatchWholeWord: false,
-                        MatchWildcards: false,
-                        MatchSoundsLike: missing,
-                        MatchAllWordForms: false,
-                        Forward: true,
-                        Wrap: wrap,
-                        Format: false,
-                        ReplaceWith: missing,
-                        Replace: replace);
-                }
-                Object newFileName = Path.Combine(_fileInfo.DirectoryName, DateTime.Now.ToString("yyyyMMdd HHmmss") + _fileInfo.Name);
-                app.ActiveDocument.SaveAs2(newFileName);
-                app.ActiveDocument.Close();
-                
-                return true;
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show($"{ex.Message}", "Ошибка") ;
-                return false;
-            }
-            finally
-            {
-                if (app != null)
-                {
-                    app.Quit();
-                }
-            }
-            throw new NotImplementedException();
-        }
-    }
-}
+   
